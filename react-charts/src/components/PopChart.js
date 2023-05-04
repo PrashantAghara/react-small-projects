de6c75a61b969978490c1331a161b1b2b0e2d449
prashant <prashant.aghara2111@gmail.com>
2023-05-04 22:41:38 +0530

    Project 3

---
+++ react-charts/src/components/PopChart.js
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import Chart from "react-apexcharts";
+
+const PopChart = () => {
+  const [options, setOptions] = useState({
+    chart: {
+      background: "#f4f4f4",
+      foreColor: "#333",
+    },
+    xaxis: {
+      categories: [
+        "New York",
+        "Los Angeles",
+        "Chicago",
+        "Houston",
+        "Philadelphia",
+        "Phoenix",
+        "San Antonio",
+        "San Diego",
+        "Dallas",
+        "San Jose",
+      ],
+    },
+    plotOptions: {
+      bar: {
+        horizontal: false,
+      },
+    },
+    fill: {
+      colors: ["#f44336"],
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    title: {
+      text: "Population",
+      align: "center",
+      margin: 20,
+      offsetY: 20,
+      style: {
+        fontSize: 25,
+      },
+    },
+  });
+  const [series] = useState([
+    {
+      name: "Population",
+      data: [
+        8550405, 3971883, 2720546, 2296224, 1567442, 1563025, 1469845, 1394928,
+        1300092, 1026908,
+      ],
+    },
+  ]);
+  const onClickHandler = () => {
+    setOptions({
+      options: {
+        ...options,
+        plotOptions: {
+          ...options.plotOptions,
+          bar: {
+            ...options.plotOptions.bar,
+            horizontal: !options.plotOptions.bar.horizontal,
+          },
+        },
+      },
+    });
+  };
+  return (
+    <>
+      <Chart
+        options={options}
+        series={series}
+        type="bar"
+        height={450}
+        width="100%"
+      />
+      <button onClick={onClickHandler}>Horizontal</button>
+    </>
+  );
+};
+
+export default PopChart;
